5c56a87630adbcaf75ec466e62dbb04f339c9e21
Gayitha Lasara Dammiya Arachchilage <gdammiya@uwo.ca>
2024-07-21 21:26:51 -0400

    Fixed issues linking CSS and JS files Fixed navigation bar transitions Fixed issue with hero image not appearing

--- js/script.js
+++ js/script.js
@@ -1,9 +1,48 @@
+// Hide Header/Navigation Bar Opacity
 const navEl = document.querySelector('.header');
+const toggleButton = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
         navEl.classList.add('bg-light');
-    } else if (window.scrollY < 56) {
+        navEl.classList.add('header-box-shadow');
+    } else {
         navEl.classList.remove('bg-light');
+        navEl.classList.remove('header-box-shadow');
     }
-})
+});
+
+toggleButton.addEventListener('click', () => {
+    if (navbarCollapse.classList.contains('show')) {
+        navEl.classList.remove('bg-light');
+        navEl.classList.remove('header-box-shadow');
+    } else {
+        navEl.classList.add('bg-light');
+        navEl.classList.add('header-box-shadow');
+    }
+});
+
+navbarCollapse.addEventListener('hidden.bs.collapse', () => {
+    if (window.scrollY < 56) {
+        navEl.classList.remove('bg-light');
+        navEl.classList.remove('header-box-shadow');
+    }
+});
+
+// Navigation Bar Dropdown
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdowns = document.querySelectorAll('.navbar-nav .dropdown');
+
+    dropdowns.forEach(function (dropdown) {
+        dropdown.addEventListener('mouseenter', function () {
+            var dropdownMenu = this.querySelector('.dropdown-menu');
+            dropdownMenu.classList.add('show');
+        });
+
+        dropdown.addEventListener('mouseleave', function () {
+            var dropdownMenu = this.querySelector('.dropdown-menu');
+            dropdownMenu.classList.remove('show');
+        });
+    });
+});
